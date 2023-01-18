4334b4e039dea27bc9158ae9fa0cdf895236663b
LiamBrem <liam.e.brem@gmail.com>
2023-01-17 19:49:44 -0500

    imported openapi & config

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+import config
+import openai
 
+#config is ignored so api key doesn't get shared
+API_KEY = config.api_key
 
 def getId(url):
     for i in range(len(url)):
@@ -25,4 +29,9 @@ def returnTranscript(id):
 
 if __name__ == "__main__":
     id = getUserInput()
-    print(returnTranscript(id))
+    transcript = returnTranscript(id)
+
+    openai.api_key = API_KEY
+
+    openai.Model.list()
+
